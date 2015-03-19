fbf6481cd763fcc374c782151048061c51c87b71
Joshua Egan <josh.egan145@gmail.com>
2015-03-18 21:54:18 -0600

    Created Contracts.Argument and implemented CannotBeNull and CannotBeNullOrEmpty

---
+++ src/Compost/Contracts/Argument.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Compost.Contracts
+{
+    /// <summary>
+    ///     An easliy testable class for ensuring that arguments are valid.
+    /// </summary>
+    public static class Argument
+    {
+        /// <summary>
+        ///     Ensures that the argument is not null. If the argument is null, an <seealso cref="ArgumentNullException" /> is
+        ///     thrown.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void CannotBeNull<T>(T arg) where T : class
+        {
+            if (arg == null)
+                throw new ArgumentNullException();
+        }
+
+        /// <summary>
+        ///     Ensures that the argument is not null. If the argument is null, an <seealso cref="ArgumentNullException" /> is
+        ///     thrown.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <param name="argumentName"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void CannotBeNull<T>(T arg, string argumentName) where T : class
+        {
+            if (arg == null)
+                throw new ArgumentNullException(argumentName);
+        }
+
+        /// <summary>
+        ///     Ensures that the argument is not null or empty. If the argument is null, an
+        ///     <seealso cref="ArgumentNullException" /> is thrown. If the argument is empty, an
+        ///     <seealso cref="ArgumentException" /> is thrown.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void CannotBeNullOrEmpty(string arg)
+        {
+            CannotBeNull(arg);
+            if (arg == string.Empty)
+                throw new ArgumentException("Argument cannot be empty!");
+        }
+
+        /// <summary>
+        ///     Ensures that the argument is not null or empty. If the argument is null, an
+        ///     <seealso cref="ArgumentNullException" /> is thrown. If the argument is empty, an
+        ///     <seealso cref="ArgumentException" /> is thrown.
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <param name="argumentName"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void CannotBeNullOrEmpty(string arg, string argumentName)
+        {
+            CannotBeNull(arg, argumentName);
+            if (arg == string.Empty)
+                throw new ArgumentException("Argument cannot be empty!", argumentName);
+        }
+    }
+}
---
+++ src/tests/Compost.Tests/Contracts/ArgumentTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Compost.Contracts;
+using Compost.Reflection;
+using NUnit.Framework;
+
+namespace Compost.Tests.Contracts
+{
+    [TestFixture]
+    public class ArgumentTests
+    {
+        // ReSharper disable ConvertToConstant.Local
+        private static readonly string NullString = null;
+        private static readonly string ValidString = "asdf";
+        private static readonly string WhitespaceString = " ";
+        private static readonly string EmptyString = "";
+        // ReSharper restore ConvertToConstant.Local
+
+        [Test]
+        public void cannot_be_null_does_not_throw_if_arg_is_not_null()
+        {
+            Argument.CannotBeNull(ValidString);
+
+            Assert.Pass("Exception was not thrown.");
+        }
+
+        [Test]
+        public void cannot_be_null_does_not_throw_if_arg_is_not_null_2()
+        {
+            Argument.CannotBeNull(ValidString, Reflector.MemberName(() => ValidString));
+
+            Assert.Pass("Exception was not thrown.");
+        }
+
+        [Test]
+        public void cannot_be_null_throws_if_argument_is_null()
+        {
+            AssertThat.ExceptionIsThrown<ArgumentNullException>(() => Argument.CannotBeNull(NullString));
+        }
+
+        [Test]
+        public void cannot_be_null_throws_if_argument_is_null_2()
+        {
+            AssertThat.ExceptionIsThrown<ArgumentNullException>(
+                () => Argument.CannotBeNull(NullString, Reflector.MemberName(() => NullString)));
+
+        }
+
+        [Test]
+        public void cannot_be_null_or_empty_does_not_throw_if_string_is_not_null_or_empty()
+        {
+            Argument.CannotBeNullOrEmpty(ValidString);
+            Argument.CannotBeNullOrEmpty(WhitespaceString);
+
+            Assert.Pass("Exception was not thrown.");
+        }
+
+        [Test]
+        public void cannot_be_null_or_empty_throws_if_argument_is_null_or_empty()
+        {
+            AssertThat.ExceptionIsThrown<ArgumentNullException>(() => Argument.CannotBeNullOrEmpty(null));
+            AssertThat.ExceptionIsThrown<ArgumentException>(() => Argument.CannotBeNullOrEmpty(""));
+        }
+
+        [Test]
+        public void cannot_be_null_or_empty_does_not_throw_if_string_is_not_null_or_empty_2()
+        {
+            Argument.CannotBeNullOrEmpty(ValidString, Reflector.MemberName(() => ValidString));
+            Argument.CannotBeNullOrEmpty(WhitespaceString, Reflector.MemberName(() => WhitespaceString));
+
+            Assert.Pass("Exception was not thrown.");
+        }
+
+        [Test]
+        public void cannot_be_null_or_empty_throws_if_argument_is_null_or_empty_2()
+        {
+            AssertThat.ExceptionIsThrown<ArgumentNullException>(() => Argument.CannotBeNullOrEmpty(NullString, Reflector.MemberName(() => NullString)));
+            AssertThat.ExceptionIsThrown<ArgumentException>(() => Argument.CannotBeNullOrEmpty(EmptyString, Reflector.MemberName(() => EmptyString)));
+        }
+
+    }
+}
