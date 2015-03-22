b928dab42dd2517420f349cc13f59fa5f5dfe932
Joshua Egan <josh.egan145@gmail.com>
2015-03-22 04:06:34 -0600

    feat(IOWrapper): Implemented GetFileName

--- src/Compost/FileSystem/IOWrapper.cs
+++ src/Compost/FileSystem/IOWrapper.cs
@@ -8,11 +8,27 @@ namespace Compost.FileSystem
     public interface IIOWrapper
     {
         /// <summary>
-        ///     Wrapper for the <seealso cref="System.IO.Path.Combine(string[])" /> method.
+        ///     Wrapper for the <seealso cref="Path.Combine(string[])" /> method.
         /// </summary>
         /// <param name="paths"></param>
         /// <returns></returns>
         string Combine(params string[] paths);
+
+        /// <summary>
+        ///     Wrapper for the <seealso cref="Path.GetExtension" /> method.
+        ///     Returns the file extension of the <paramref name="filePath" />. (e.g. ".txt", ".jpg")
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        string GetFileExtension(string filePath);
+
+        /// <summary>
+        ///     Wrapper for the <seealso cref="Path.GetFileName" /> method.
+        ///     Returns the file name and extension from the <paramref name="filePath" />.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        string GetFileName(string filePath);
     }
 
     /// <summary>
@@ -40,6 +56,17 @@ namespace Compost.FileSystem
         public string GetFileExtension(string filePath)
         {
             return Path.GetExtension(filePath);
+        }
+
+        /// <summary>
+        ///     Wrapper for the <seealso cref="Path.GetFileName" /> method.
+        ///     Returns the file name and extension from the <paramref name="filePath" />.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public string GetFileName(string filePath)
+        {
+            return Path.GetFileName(filePath);
         }
     }
 }
