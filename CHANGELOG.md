d10ff5608a6eea27ed02f0f2b22cdd9a6ae938dd
Joshua Egan <josh.egan145@gmail.com>
2015-03-22 02:55:12 -0600

    chore(*): Attempting to debug build script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,3 @@
-<a name"0.2.1"></a>
-### 0.2.1 (2015-03-22)
-
-
-#### Features
-
-* **gulpfile:** Add ability to genrate changelogs ([4ae883fa](https://github.com/eganj/Compost.NET/commit/4ae883fa))
+<a name"0.3.0"></a>
+## 0.3.0 (2015-03-22)
 
--- UpdateVersionThenPublish.bat
+++ UpdateVersionThenPublish.bat
@@ -9,15 +9,16 @@ call %msBuildDir%\MSBuild.exe  Compost.NET.sln /p:Configuration=Release
 
 :: Generate the changelog
 :: changelog step consumes version.json and creates release_version.txt
-gulp changelog
+gulp changelog > debug.txt
+pause
 
 :: Publish to GitHub releases
 set /p API_KEY=< GitHubApiKey.txt
 set /p VERSION=< release_version.txt  
 GithubReleaseCreator_1.1.0\GithubReleaseCreator.exe --username %API_KEY% --owner "eganj" --repo "compost.net" --tag %TAG% --name %VERSION% --filedesc "CHANGELOG.md" -assetfiles "bin\Release\Compost.dll,bin\Release\Compost.pdb,bin\Release\Compost.XML" --verbose
+pause
 
 :: Publish to nuget
 nuget pack ./src/Compost/Compost.csproj -Prop Configuration=Release
 nuget push ./Compost.nupkg
-
 pause
