1803f9ffc157f9fe4e41af24577b06ba47eefa5b
Joshua Egan <josh.egan145@gmail.com>
2015-03-22 03:01:05 -0600

    feat(UpdateVersionThenPublish): does changelog require a certain type of commit

--- UpdateVersionThenPublish.bat
+++ UpdateVersionThenPublish.bat
@@ -15,7 +15,7 @@ pause
 :: Publish to GitHub releases
 set /p API_KEY=< GitHubApiKey.txt
 set /p VERSION=< release_version.txt  
-GithubReleaseCreator_1.1.0\GithubReleaseCreator.exe --username %API_KEY% --owner "eganj" --repo "compost.net" --tag %TAG% --name %VERSION% --filedesc "CHANGELOG.md" -assetfiles "bin\Release\Compost.dll,bin\Release\Compost.pdb,bin\Release\Compost.XML" --verbose
+GithubReleaseCreator_1.1.0\GithubReleaseCreator.exe --username %API_KEY% --owner "eganj" --repo "compost.net" --name %VERSION% --filedesc "CHANGELOG.md" -assetfiles "bin\Release\Compost.dll,bin\Release\Compost.pdb,bin\Release\Compost.XML" --verbose
 pause
 
 :: Publish to nuget
