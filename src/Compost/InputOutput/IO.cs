17d7d3130ea5c707d176be85a8bfb2b371488965
Joshua Egan <josh.egan145@gmail.com>
2015-04-01 03:31:56 -0600

    doc(IO): Updated docs

--- src/Compost/InputOutput/IO.cs
+++ src/Compost/InputOutput/IO.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using Compost.Reflection;
 
 namespace Compost.InputOutput
@@ -28,7 +27,7 @@ namespace Compost.InputOutput
         }
 
         /// <summary>
-        ///     Wrapper for the <seealso cref="Path.Combine(string[])" /> method.
+        ///     Combines an array of strings into a path.
         /// </summary>
         /// <param name="paths"></param>
         /// <returns></returns>
@@ -38,7 +37,6 @@ namespace Compost.InputOutput
         }
 
         /// <summary>
-        ///     Wrapper for the <seealso cref="Path.GetDirectoryName" /> method.
         ///     Returns the full path of the containing directory of the <paramref name="path" />.
         ///     For example, "C:\some\file\path.txt" will return "C:\some\file" and
         ///     "C:\some\dir" will return "C:\some"
@@ -51,7 +49,6 @@ namespace Compost.InputOutput
         }
 
         /// <summary>
-        ///     Wrapper for the <seealso cref="Path.GetExtension" /> method.
         ///     Returns the file extension of the <paramref name="filePath" />. (e.g. ".txt", ".jpg")
         /// </summary>
         /// <param name="filePath"></param>
@@ -62,7 +59,6 @@ namespace Compost.InputOutput
         }
 
         /// <summary>
-        ///     Wrapper for the <seealso cref="Path.GetFileName" /> method.
         ///     Returns the file name and extension from the <paramref name="filePath" />.
         /// </summary>
         /// <param name="filePath"></param>
@@ -73,7 +69,6 @@ namespace Compost.InputOutput
         }
 
         /// <summary>
-        ///     Wrapper for the <seealso cref="Path.GetFileNameWithoutExtension" /> method.
         ///     Returns the file name without the extension from the <paramref name="filePath" />.
         /// </summary>
         /// <param name="filePath"></param>
@@ -84,7 +79,6 @@ namespace Compost.InputOutput
         }
 
         /// <summary>
-        ///     Wrapper for the <seealso cref="Path.GetFullPath" /> method.
         ///     Returns the full path of the <paramref name="path" />.
         /// </summary>
         /// <param name="path"></param>
